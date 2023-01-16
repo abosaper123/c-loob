dfcfb4a62661f886ced3444761dc2ef9470f6bc5
boda <abosaper123@gmail.com>
2023-01-16 23:06:29 +0200

    please study c++

---
+++ Project20/Project20/Source.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+	int i;
+	for (i=0;i<99;i++)
+	{
+		if ( i%2!=0)
+		{
+			cout << "this is odd number "<< i;
+		}
+	}
+	system ("pause");
+	return 0;
+}
